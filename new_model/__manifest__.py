eb315521fe6a1e4e54b6d02930c513af57b46ac8
ashokpk <ashokpkmaniyara@gmail.com>
2023-12-16 12:15:36 +0530

    creating new model and fields without python

---
+++ new_model/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'new model',
+    'version': '16.0.1.0.0',
+    'depends': ['base'],
+    'author': "Ashokpk",
+    'data':[
+        'views/new_model.xml',
+        'security/ir.model.access.csv',
+    ],
+}
